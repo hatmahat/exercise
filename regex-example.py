288062322deebbecff2c4a523e423bbfa5573acc
hatmahat <56577748+hatmahat@users.noreply.github.com>
2020-06-04 17:56:00 +0700

    Add files via upload

--- regex-example.py
+++ regex-example.py
@@ -34,3 +34,23 @@ print(
         flight_matches[0][4]
     )
 )
+
+html_tags = [
+    '<body> hello world! </body>',
+    '<article> bye2 world! </article>',
+    '<nav> heh3h3',
+    '<h1> test2'
+]
+
+for string in html_tags:
+    # Complete the regex and find if it matches a closed HTML tags
+    match_tag =  re.match(r"<(\w+)>.*?</\1>", string)
+    #print(match_tag)
+    if match_tag:
+        # If it matches print the first group capture
+        print("Your tag {} is closed".format(match_tag.group(1))) 
+    else:
+        # If it doesn't match capture only the tag 
+        notmatch_tag = re.match(r"<(\w+)>", string)
+        # Print the first group capture
+        print("Close your {} tag!".format(notmatch_tag.group(1)))
